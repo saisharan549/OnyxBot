39dfccd72bf09b5ffd578dac2886326019ba53d7
predator777alpha <70087721+predator777alpha@users.noreply.github.com>
2021-01-20 12:01:35 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,10 +26,10 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running OtakuBot [v1.1]")
+LOGS.info("You are running Onyx [v1.1]")
 
 LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .bot / .on in any chat."
+    "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
     "If you need assistance, Go to hell!!!!")
 
 if len(argv) not in (1, 3, 4):
