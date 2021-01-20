7a3a5dab1d97a0c2f9e40289be01d0570750824a
predator777alpha <70087721+predator777alpha@users.noreply.github.com>
2021-01-20 12:29:59 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,7 +26,7 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running Onyx [v1.1]")
+LOGS.info("You are running Onyx [v1.0]")
 
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
