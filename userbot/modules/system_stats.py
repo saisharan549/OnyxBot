62aa3de9a01b438482d1a8de10fc6bf0f95e2f77
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-01-20 19:38:35 +0530

    minor alive changes

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -231,13 +231,13 @@ async def amireallyalive(alive):
     output = (
 "`========================================`\n"
         "`              OnyxBot           `\n"
-        f"• `Telethon       : v{version.__version__} `\n"
-        f"• `Python         : v{python_version()} `\n"
-        f"• `User           :`  {DEFAULTUSER} \n"
-        f"• `Running on     : {repo.active_branch.name} `\n"
-        f"• `Loaded modules : {len(modules)} `\n"
-        f"• `Bot_version    : v{USERBOT_VERSION} `\n"
-        f"• `Bot Uptime     : {uptime} `\n"
+        f"• **Telethon       : v{version.__version__} **\n"
+        f"• **Python         : v{python_version()} **\n"
+        f"• **User           : {DEFAULTUSER} **\n"
+        f"• **Running on     : {repo.active_branch.name} **\n"
+        f"• **Loaded modules : {len(modules)} **\n"
+        f"• **Bot_version    : v{USERBOT_VERSION} **\n"
+        f"• **Bot Uptime     : {uptime} **\n"
 "`==========================================`\n"    )
     if ALIVE_LOGO:
         try:
